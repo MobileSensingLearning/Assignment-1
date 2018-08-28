bfcb47b1a05988f874df882b372944a3620dea01
Brandon McFarland <branmcf@gmail.com>
2018-08-28 17:28:44 -0500

    Initial Commit

---
+++ 5323_assignment_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  5323_assignment_1
+//
+//  Created by Brandon McFarland on 8/28/18.
+//  Copyright © 2018 MobileSensingLearning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
