ff3caa3473569bd03a81fceb74d5a86ea73b02dd
John Doe <slefcourt12@gmail.com>
2018-09-06 12:49:10 -0500

    Initial Commit

---
+++ hw1bullseye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hw1bullseye
+//
+//  Created by Samuel Lefcourt on 9/4/18.
+//  Copyright © 2018 Saminator5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
